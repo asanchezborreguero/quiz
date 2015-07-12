61a03629d55c3dfe852ec8eb8166160095ab90fb
unknown <asanchez@PC1083P.indimedia.com>
2015-07-12 20:23:35 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+
 router.get ('/quizes/question', quizController.question);
 router.get ('/quizes/answer',   quizController.answer);
+router.get('/author', function (req, res, next) 
+	{res.render('author', {});
+});
 
 module.exports = router;
